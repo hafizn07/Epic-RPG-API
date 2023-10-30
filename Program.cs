a6b64bd649f39ca850f832ebf87b367a3692e7a3
hafizmp <hafiznjh007@gmail.com>
2023-10-30 20:46:16 +0530

    feat: ConnectionString & Adding the DbContext

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@ global using Epic_RPG_API.Services.CharacterService;
 global using Epic_RPG_API.Dtos.Character;
 global using AutoMapper;
 global using Microsoft.EntityFrameworkCore;
+global using Epic_RPG_API.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
