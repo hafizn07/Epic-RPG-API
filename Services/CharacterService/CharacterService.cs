66bf10f8489a93b8da770cfdae5d2641078bc185
hafizmp <hafiznjh007@gmail.com>
2023-11-01 11:44:13 +0530

    feat: Include Entities with Entity Framework

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -85,8 +85,10 @@ namespace Epic_RPG_API.Services.CharacterService
 
             try
             {
-                var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
-                if (character is null)
+                var character = await _context.Characters
+                    .Include(c => c.User)
+                    .FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+                if (character is null || character.User!.Id != GetUserId())
                     throw new Exception($"Character with Id '{updatedCharacter.Id}' not found.");
 
                 character.Name = updatedCharacter.Name;
