3fa46ff68f6ebbaf5e65d951cafc1fb773a4d96f
hafizmp <hafiznjh007@gmail.com>
2023-11-01 14:26:22 +0530

    feat: Data Seeding - Add Skills to the DB

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -11,6 +11,28 @@ namespace be_rpg.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill { Id = 1, Name = "Fireball", Damage = 70 },
+                new Skill { Id = 2, Name = "Arcane Shield", Damage = 30 },
+                new Skill { Id = 3, Name = "Teleport", Damage = 40 },
+                new Skill { Id = 4, Name = "Cleave", Damage = 85 },
+                new Skill { Id = 5, Name = "Shield Bash", Damage = 50 },
+                new Skill { Id = 6, Name = "Whirlwind Attack", Damage = 70 },
+                new Skill { Id = 7, Name = "Backstab", Damage = 75 },
+                new Skill { Id = 8, Name = "Evasion", Damage = 40 },
+                new Skill { Id = 9, Name = "Shadowstep", Damage = 60 },
+                new Skill { Id = 10, Name = "Raging Strike", Damage = 90 },
+                new Skill { Id = 11, Name = "Holy Strike", Damage = 75 },
+                new Skill { Id = 12, Name = "Assassinate", Damage = 95 },
+                new Skill { Id = 13, Name = "Frenzy", Damage = 100 },
+                new Skill { Id = 14, Name = "Bloodlust", Damage = 85 },
+                new Skill { Id = 15, Name = "Telepathy", Damage = 45 }
+                );
+        }
+
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<User> Users => Set<User>();
         public DbSet<Weapon> Weapons => Set<Weapon>();
---
+++ Migrations/20231101085505_SkillSeeding.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Epic_RPG_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SkillSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "Damage", "Name" },
+                values: new object[,]
+                {
+                    { 1, 70, "Fireball" },
+                    { 2, 30, "Arcane Shield" },
+                    { 3, 40, "Teleport" },
+                    { 4, 85, "Cleave" },
+                    { 5, 50, "Shield Bash" },
+                    { 6, 70, "Whirlwind Attack" },
+                    { 7, 75, "Backstab" },
+                    { 8, 40, "Evasion" },
+                    { 9, 60, "Shadowstep" },
+                    { 10, 90, "Raging Strike" },
+                    { 11, 75, "Holy Strike" },
+                    { 12, 95, "Assassinate" },
+                    { 13, 100, "Frenzy" },
+                    { 14, 85, "Bloodlust" },
+                    { 15, 45, "Telepathy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 15);
+        }
+    }
+}
